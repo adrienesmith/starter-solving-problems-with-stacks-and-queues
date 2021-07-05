ac16cf8bdd947c7cf6f6fd2857e8b886ff596fc5
Adriene Smith <adriene.e.smith@gmail.com>
2021-07-05 08:04:28 -0700

    palindromes and parentheses completed

--- src/palindromes/palindromes.js
+++ src/palindromes/palindromes.js
@@ -2,7 +2,15 @@ const Stack = require("../lib/stack");
 
 const isPalindrome = (sentence) => {
   sentence = sentence.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
-  // Your solution here
+  const middle = Math.floor(sentence.length / 2);
+  const stack = new Stack();
+  for (let i = 0; i < middle; i++){
+    stack.push(sentence[i]);
+  }
+  for (let i = sentence.length % 2 === 0 ? middle : middle + 1; i < sentence.length; i++) {
+    if (stack.pop() !== sentence[i]) return false;
+  }
+  return true;
 };
 
 module.exports = isPalindrome;
--- src/parentheses/parentheses.js
+++ src/parentheses/parentheses.js
@@ -1,5 +1,20 @@
 const Stack = require("../lib/stack");
 
-const match = (expression) => {};
-
+const match = (expression) => {
+    const stack = new Stack();
+    for (let i = 0; i < expression.length; i++){
+        if (expression[i] === "(") {
+            stack.push(expression[i]);
+        }
+        if (expression[i] === ")") {
+            if (stack.top) {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }    
+    }
+    if (!stack.top) return true;
+    return false;
+};
 module.exports = match;
